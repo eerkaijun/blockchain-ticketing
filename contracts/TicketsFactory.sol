0fc78e0eb01de5220734ce293f5ccfe8d7a52c7d
Eer Kai Jun <kaijuneer@gmail.com>
2020-05-21 05:00:32 +0100

    initialise

---
+++ contracts/TicketsFactory.sol
@@ -0,0 +1,23 @@
+pragma solidity >=0.6.0 <0.7.0;
+
+contract TicketsFactory {
+
+  uint private id = 0; //might be gas expensive as a storage variable? 
+
+  struct Ticket {
+    uint32 identifier;
+    string seatNumber;
+  }
+
+  //@dev list of tickets for the event, with index of tickets as its id
+  Ticket[] public tickets;
+
+  mapping(uint => address) public ticketToOwner; //maps ticket id to owner
+
+  function _createTickets(uint32 _identifier, string memory _seatNumber) private {
+    tickets.push(Ticket(_identifier, _seatNumber));
+    ticketToOwner[id] = msg.sender;
+    id++;
+  }
+
+}
