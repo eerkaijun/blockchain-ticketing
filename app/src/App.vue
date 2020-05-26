686544690c64a65e3e5ce3887115e35cf1d6746d
Eer Kai Jun <kaijuneer@gmail.com>
2020-05-26 05:23:31 +0100

    set up provider for frontend

--- app/src/App.vue
+++ app/src/App.vue
@@ -28,13 +28,46 @@
   </v-app>
 </template>
 
+<script src="jslibs/web3.min.js"></script>
+<script src="jslibs/truffle-contract.js"></script>
 <script>
+
 export default {
   name: 'App',
   data () {
     return {
-      //
+      web3Provider: null,
+      contracts: {},
+      account: '0x0',
     }
+  },
+
+  async mounted() {
+    await this.initProvider();
+    //this.initContract();
+  },
+
+  methods: {
+    async initProvider() {
+      if (typeof web3 !== 'undefined') {
+        this.web3Provider = web3.currentProvider;
+        ethereum.enable();
+        web3 = new Web3(web3.currentProvider);
+        this.account = web3.eth.accounts[0];
+      } else {
+        console.log("Please install Metamask to continue.");
+      }
+
+      setInterval(function() {
+        // Check if account has changed
+        if (web3.eth.accounts[0] !== this.account) {
+          this.account = web3.eth.accounts[0];
+          // Call a function to update the UI with the new account
+          alert("You changed account!");
+        }
+      }, 100);
+    }
+
   }
 }
 </script>
