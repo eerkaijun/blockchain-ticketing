2a0b3107406cb147ef59d2545ef5d6cb48f1ee3c
eerkaijun <kaijuneer@gmail.com>
2020-09-23 12:15:26 +0100

    migration for marketplace

--- migrations/2_deploy_tickets_factory.js
+++ migrations/2_deploy_tickets_factory.js
@@ -1,5 +1,7 @@
 const TicketsFactory = artifacts.require("TicketsFactory");
+const Marketplace = artifacts.require("Marketplace");
 
 module.exports = function(deployer) {
   deployer.deploy(TicketsFactory);
+  deployer.deploy(Marketplace);
 };
