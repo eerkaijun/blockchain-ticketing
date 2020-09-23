ba54b28303af0a8369b32a47a711af2a3011c927
eerkaijun <kaijuneer@gmail.com>
2020-09-23 12:44:03 +0100

    passing test

--- test/TicketsFactory.js
+++ test/TicketsFactory.js
@@ -9,16 +9,16 @@ contract("TicketsFactory", (accounts) => {
   });
 
   it("Owner should be able to create tickets", async() => {
-    const result = await contractInstance.createTickets(1, "A01", {from:owner});
+    const result = await contractInstance.createTicket(3, "A01", {from:owner});
     assert.equal(result.receipt.status, true);
     const ownerAddress = await contractInstance.ticketToOwner(0);
     assert.equal(ownerAddress, owner);
     const ticket = await contractInstance.tickets(0);
-    assert.equal(ticket[0], 1, "identifer is correct");
+    assert.equal(ticket[0], 3, "ticket price is correct");
     assert.equal(ticket[1], "A01", "seat number is correct");
   });
 
   it("Normal user should not be able to create tickets", async() => {
-    await utils.shouldThrow(contractInstance.createTickets(1, "A01", {from:user}));
+    await utils.shouldThrow(contractInstance.createTicket(3, "A01", {from:user}));
   });
 })
