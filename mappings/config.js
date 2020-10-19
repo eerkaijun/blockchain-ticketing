27885be0a28365b9feed5588c31060cdfafd7e7b
eerkaijun <kaijuneer@gmail.com>
2020-10-19 13:00:19 +0100

    transferring tokens between chains

---
+++ mappings/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: {
+    RPC: 'https://goerli.infura.io/v3/3275f4ffdd0d4b3e8ac104b1bd78007d',
+    POSRootChainManager: '0xBbD7cBFA79faee899Eaf900F13C9065bF03B1A74',
+    DERC721: '0xcAb28D1d44B033dedB0aFE3150378f1C39Eff7e4',
+    posERC721Predicate: '0x74D83801586E9D3C4dc45FfCD30B54eA9C88cf9b',
+  },
+  child: {
+    RPC: 'https://rpc-mumbai.matic.today',
+    DERC721: '0x22Fc73bC6Af889A0Adb5405f126a600Ac3Cb4651',
+  },
+  user: {
+    address: '0xEDB4400a8b1DEccc6C62DFDDBD6F73E48537012A',
+    tokenId: '1',
+  },
+}
