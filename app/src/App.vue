5d94701a90604e9523ac2b9e446ceb01f48e3952
eerkaijun <kaijuneer@gmail.com>
2020-10-30 12:47:41 +0000

    frontend event organisers create tickets

--- app/src/App.vue
+++ app/src/App.vue
@@ -44,9 +44,12 @@
   </v-app>
 </template>
 
-<script src="jslibs/web3.min.js"></script>
-<script src="jslibs/truffle-contract.js"></script>
+<script language="javascript" type="text/javascript" src="jslibs/web3.min.js"></script>
 <script>
+
+import MarketplaceABI from './plugins/Marketplace';
+
+const Web3 = require('web3');
 
 export default {
   name: 'App',
@@ -67,8 +70,8 @@ export default {
     async initProvider() {
       if (typeof web3 !== 'undefined') {
         this.web3Provider = web3.currentProvider;
-        ethereum.enable();
-        web3 = new Web3(web3.currentProvider);
+        web3 = new Web3(web3.currentProvider); //force it to version 1.2.7
+        console.log(web3.version);
         this.account = web3.eth.accounts[0];
       } else {
         console.log("Please install Metamask to continue.");
@@ -86,14 +89,13 @@ export default {
 
     async initContract() {
       const contractAddress = "0x22Fc73bC6Af889A0Adb5405f126a600Ac3Cb4651"; //Mumbai testnet address
-      const contractInstance = await fetch('Marketplace.json');
-      console.log(contractInstance);
-      this.contract = await web3.eth.contract(contractInstance, contractAddress);
+      this.contract = await new web3.eth.Contract(MarketplaceABI, contractAddress);
+      console.log(this.account);
     },
 
     async createTicket(price) {
-      //var ticketPrice = await web3.utils.toWei(price, 'ether');
-      await this.contract.methods.createTicket(price);
+      console.log(this.contract);
+      await this.contract.methods.createTicket(price).send({from:'0xEDB4400a8b1DEccc6C62DFDDBD6F73E48537012A'});
       console.log("Ticket created successfully!");
     }
 
