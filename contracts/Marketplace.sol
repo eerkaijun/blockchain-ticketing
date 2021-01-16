f8e6cee68484b2493548a4d05260d00e1cf5b252
eerkaijun <kaijuneer@gmail.com>
2021-01-16 10:40:48 +0000

    change ticket price

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -1,11 +1,11 @@
 pragma solidity >=0.6.0 <0.7.0;
 
 import "./TicketsFactory.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
+//import "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract Marketplace is TicketsFactory{
 
-  using SafeMath for uint;
+  //using SafeMath for uint;
   mapping (address => uint256) etherBalance;
   uint private _maxTicketNum;
 
@@ -29,13 +29,13 @@ contract Marketplace is TicketsFactory{
     uint256 price = ticketPrice[_tokenId];
     require(onSale[_tokenId] == true);
     require(msg.value >= price, "at least the ticket price");
-    require(msg.value < price.add(price.div(10)), "not more than 10% of original price");
+    //require(msg.value < price.add(price.div(10)), "not more than 10% of original price");
     require(balanceOf(msg.sender) <= _maxTicketNum, "exceeded max number of tickets bought");
     address seller = ownerOf(_tokenId);
-    _safeTransfer(seller, msg.sender, _tokenId, "");
     onSale[_tokenId] = false;
     etherBalance[seller] += msg.value;
     owners[_tokenId] = msg.sender;
+    _safeTransfer(seller, msg.sender, _tokenId, "");
     emit ticketTransferred(_tokenId, msg.sender);
     emit saleToggled(_tokenId, false);
   }
--- contracts/TicketsFactory.sol
+++ contracts/TicketsFactory.sol
@@ -2,11 +2,15 @@ pragma solidity >=0.6.0 <0.7.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract TicketsFactory is Ownable, ERC721{
 
+  using SafeMath for uint;
+
   uint256 private _currentTokenId = 0;
   mapping (uint256 => uint256) public ticketPrice;
+  mapping (uint256 => uint256) public originalTicketPrice;
   //mapping (uint256 => bool) onSale;
   bool[] public onSale;
   address[] public owners;
@@ -19,13 +23,17 @@ contract TicketsFactory is Ownable, ERC721{
     //uint256 newTokenId = _getNextTokenId();
     _mint(msg.sender, _currentTokenId); //token id starts from 0
     ticketPrice[_currentTokenId] = _price;
+    originalTicketPrice[_currentTokenId] = _price;
     onSale.push(false);
     owners.push(msg.sender);
     _setTokenURI(_currentTokenId, _tokenURI);
     _incrementTokenId();
   }
 
-  function changeTicketPrice(uint256 _tokenId, uint256 _price, string memory _tokenURI) public onlyOwner {
+  function changeTicketPrice(uint256 _tokenId, uint256 _price, string memory _tokenURI) public {
+    require(msg.sender == ownerOf(_tokenId));
+    uint256 originalPrice = originalTicketPrice[_tokenId];
+    require(_price < originalPrice.add(originalPrice.div(10)), "not more than 10% of original price");
     ticketPrice[_tokenId] = _price;
     _setTokenURI(_tokenId, _tokenURI);
   }
