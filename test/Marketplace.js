9500730dd3f16543120f0e6e36e63913103b4aec
eerkaijun <kaijuneer@gmail.com>
2021-01-30 18:05:57 +0000

    more unit tests

--- test/Marketplace.js
+++ test/Marketplace.js
@@ -9,7 +9,7 @@ contract("Marketplace", (accounts) => {
   });
 
   it("User should be able to buy ticket when on sale", async() => {
-    await contractInstance.createTicket(web3.utils.toWei('3','ether'), {from:owner});
+    await contractInstance.createTicket(web3.utils.toWei('3','ether'), "", {from:owner});
     await contractInstance.toggleSale(0, {from:owner});
     const result = await contractInstance.buyTicket(0, {from:user, value: web3.utils.toWei('3','ether')});
     assert.equal(result.receipt.status, true);
@@ -24,29 +24,29 @@ contract("Marketplace", (accounts) => {
   });
 
   it("User should not be able to buy ticket not on sale", async() => {
-    await contractInstance.createTicket(web3.utils.toWei('3','ether'), {from:owner});
+    await contractInstance.createTicket(web3.utils.toWei('3','ether'), "", {from:owner});
     await utils.shouldThrow(contractInstance.buyTicket(0, {from:user, value: web3.utils.toWei('3','ether')}));
   });
 
   it("User should not be able to buy ticket below the price", async() => {
-    await contractInstance.createTicket(web3.utils.toWei('3','ether'), {from:owner});
+    await contractInstance.createTicket(web3.utils.toWei('3','ether'), "", {from:owner});
     await contractInstance.toggleSale(0, {from:owner});
     await utils.shouldThrow(contractInstance.buyTicket(0, {from:user, value: web3.utils.toWei('2','ether')}));
   });
 
-  it("User should not be able to buy ticket above 10% of original price", async() => {
-    await contractInstance.createTicket(web3.utils.toWei('3','ether'), {from:owner});
+  it("User should not be able to buy ticket after event started", async() => {
+    await contractInstance.createTicket(web3.utils.toWei('3','ether'), "", {from:owner});
     await contractInstance.toggleSale(0, {from:owner});
-    await utils.shouldThrow(contractInstance.buyTicket(0, {from:user, value: web3.utils.toWei('3.4','ether')}));
+    await contractInstance.startEvent({from:owner});
+    await utils.shouldThrow(contractInstance.buyTicket(0, {from:user, value: web3.utils.toWei('3','ether')}));
   });
 
   it("Seller should be able to withdraw money from contract", async() => {
-    await contractInstance.createTicket(web3.utils.toWei('3','ether'), {from:owner});
+    await contractInstance.createTicket(web3.utils.toWei('3','ether'), "", {from:owner});
     await contractInstance.toggleSale(0, {from:owner});
     const result = await contractInstance.buyTicket(0, {from:user, value: web3.utils.toWei('3','ether')});
     assert.equal(result.receipt.status, true);
     await contractInstance.withdraw({from:owner});
-    // to be added: proving money is transferred into seller's account
   })
 
 })
--- test/TicketsFactory.js
+++ test/TicketsFactory.js
@@ -9,7 +9,7 @@ contract("TicketsFactory", (accounts) => {
   });
 
   it("Owner should be able to create tickets", async() => {
-    const result = await contractInstance.createTicket(web3.utils.toWei('3','ether'), {from:owner});
+    const result = await contractInstance.createTicket(web3.utils.toWei('3','ether'), "", {from:owner});
     assert.equal(result.receipt.status, true);
     const ownerAddress = await contractInstance.ownerOf(0);
     assert.equal(ownerAddress, owner, "minted tokens now owned by owner");
@@ -19,5 +19,18 @@ contract("TicketsFactory", (accounts) => {
 
   it("Normal user should not be able to create tickets", async() => {
     await utils.shouldThrow(contractInstance.createTicket(0, {from:user}));
+  });
+
+  it("Ticket owner should be able to change ticket price", async() => {
+    await contractInstance.createTicket(web3.utils.toWei('3','ether'), "", {from:owner});
+    const result = await contractInstance.changeTicketPrice(0, web3.utils.toWei('3.1','ether'), "", {from:owner});
+    assert.equal(result.receipt.status, true);
+    const ticketPrice = await contractInstance.ticketPrice(0);
+    assert.equal(web3.utils.toWei('3.1','ether'), ticketPrice, "ticket price correctly changed");
+  });
+
+  it("Ticket owner should not be able to change ticket price to higher than 10% of orginal price", async() => {
+    await contractInstance.createTicket(web3.utils.toWei('3','ether'), "", {from:owner});
+    await utils.shouldThrow(contractInstance.changeTicketPrice(0, web3.utils.toWei('3.4','ether'), "", {from:owner}));
   });
 })
