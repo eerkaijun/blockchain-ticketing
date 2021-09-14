267f5d9276f59a50f861b87f916caeb1fc96a11b
Kai Jun Eer <kaijuneer@gmail.com>
2021-09-14 14:54:47 +0100

    a better structure

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -1,43 +1,79 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.7.0 <0.8.0;
+pragma solidity ^0.8.0;
 
-import "./TicketsFactory.sol";
-//import "@openzeppelin/contracts/math/SafeMath.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
 
-contract Marketplace is TicketsFactory{
+contract Marketplace is Ownable, ERC721{
 
   using SafeMath for uint;
+  
+  // metadata for each ticket
+  struct Ticket {
+      uint256 maxPrice; // maximum price cap on the ticket
+      uint256 price; // current price
+      bool onSale;
+  }
 
+  uint256 private _currentTokenId = 0;
+  Ticket[] public tickets; 
+  bool eventStarted = false;
   mapping (address => uint256) etherBalance;
   uint private _maxTicketNum = 10;
 
-  constructor() {
-    //_maxTicketNum = 10;
+  constructor() ERC721("NFT Tickets", "TIX") {
+    
   }
 
+  modifier eventNotStarted() {
+    require(eventStarted == false);
+    _;
+  }
+
+  function createTicket(uint256 _price, uint256 _maxPrice) public onlyOwner {
+    _mint(msg.sender, _currentTokenId); //token id starts from 0
+    tickets.push(Ticket(_maxPrice, _price, false));
+    _incrementTokenId();
+  }
+
+  function changeTicketPrice(uint256 _tokenId, uint256 _newPrice) public {
+    require(msg.sender == ownerOf(_tokenId));
+    Ticket storage tix = tickets[_tokenId];
+    require(_newPrice < tix.maxPrice, "not more than the upper limit price");
+    tix.price = _newPrice;
+  }
+
+  function startEvent() public onlyOwner {
+    eventStarted = true;
+  }
+
+  function _incrementTokenId() private {
+    _currentTokenId++;
+  }
+  
   function withdraw() public {
-    msg.sender.transfer(etherBalance[msg.sender]);
+    payable(msg.sender).transfer(etherBalance[msg.sender]);
   }
 
   // tickets need to put on sale first before it could be purchased
   function toggleSale(uint256 _tokenId) public {
     require(msg.sender == ownerOf(_tokenId));
-    onSale[_tokenId] = !onSale[_tokenId];
-    emit saleToggled(_tokenId, onSale[_tokenId]);
+    Ticket storage tix = tickets[_tokenId];
+    tix.onSale = !tix.onSale;
+    emit saleToggled(_tokenId, tix.onSale);
   }
 
   // this function should be called by the buyer
   function buyTicket(uint256 _tokenId) external payable eventNotStarted {
-    uint256 price = ticketPrice[_tokenId];
-    require(onSale[_tokenId] == true);
-    require(msg.value >= price, "at least the ticket price");
-    //require(msg.value < price.add(price.div(10)), "not more than 10% of original price");
+    Ticket storage tix = tickets[_tokenId];
+    require(tix.onSale == true);
+    require(msg.value >= tix.price, "at least the ticket price");
     require(balanceOf(msg.sender) < _maxTicketNum, "exceeded max number of tickets bought");
     address seller = ownerOf(_tokenId);
-    onSale[_tokenId] = false;
+    tix.onSale = false;
     etherBalance[seller] += msg.value;
-    owners[_tokenId] = msg.sender;
     _safeTransfer(seller, msg.sender, _tokenId, "");
     emit ticketTransferred(_tokenId, msg.sender);
     emit saleToggled(_tokenId, false);
--- migrations/2_deploy_marketplace.js
+++ migrations/2_deploy_marketplace.js
@@ -1,7 +1,5 @@
-const TicketsFactory = artifacts.require("TicketsFactory");
 const Marketplace = artifacts.require("Marketplace");
 
 module.exports = function(deployer) {
-  deployer.deploy(TicketsFactory);
   deployer.deploy(Marketplace);
 };
