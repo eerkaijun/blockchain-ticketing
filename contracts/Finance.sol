5283005bcbe03b5973d7ed2cf414344645f10bd1
Kai Jun Eer <kaijuneer@gmail.com>
2021-09-19 15:55:57 +0100

    we don't actually need erc721

--- contracts/Finance.sol
+++ contracts/Finance.sol
@@ -1,19 +1,19 @@
 // SPDX-License-Identifier: MIT
+// curren deployed address on Shibuya: 0x5D1ce7Fb931274Aa315707aEB2abAD9D138780FA
 
 pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
 
-contract Finance is ERC721, Ownable {
+contract Finance is Ownable {
     
     using SafeMath for uint;
     
     uint public investmentPrice;
     uint public totalInvestment; // total shares that are released
     uint private _investmentSold = 0;
-    mapping(address=>uint) investors; // mapping to show number of shares owned by each investor
+    mapping(address=>uint) public investors; // mapping to show number of shares owned by each investor
     
     // let's say the event organiser will have 100 tickets as Collateral
     // each ticket will cost $100 in the primary market
@@ -21,8 +21,9 @@ contract Finance is ERC721, Ownable {
     // total collateral worth $40 x 100 (if sold out) = $4000
     // and now the event organiser wants to pre finance $3000
     // so the event organiser will sell each collateral at $30 until 100 collateral has been sold
-    constructor() ERC721("Collateral", "COL") {
+    constructor() {
         investmentPrice = 30;
+        totalInvestment = 100;
     }
     
     // investors can choose to invest in a number of tokens
