01de40b31b348e539dfb083f83c82cd686c1e8fa
Mariia Maliarets <mmaliarets@gmail.com>
2021-10-15 18:17:07 +0300

    show  retrieve only  when investorUnits > 0

--- react-ui-app/src/components/Content.js
+++ react-ui-app/src/components/Content.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import {
   Button,
@@ -52,155 +52,138 @@ class Content extends Component {
           <div className="col">Nice cinema places picture here</div>
         </div>
 
-        {this.props.marketplaceState ? (
-          <div className="row">
-            {/* creatingTickets, investmentStart, investmentStop, ticketSaleStart, eventStart  */}
-            {this.props.marketplaceState == "creatingTickets" ? (
-              <div className="col">
-                <Button
-                  variant="primary"
-                  onClick={(e) => {
-                    startInvestment(
-                      this.props.dispatch,
-                      this.props.marketplace,
-                      this.props.account
-                    );
-                  }}
-                  className="action-button"
-                  style={{ float: "right" }}
-                >
-                  Start Investment
-                </Button>
-              </div>
-            ) : (
-              <div></div>
-            )}
-
-            {this.props.marketplaceState == "investmentStart" ? (
-              <div className="col">
-                <Button
-                  variant="primary"
-                  onClick={(e) => {
-                    stopInvestment(
-                      this.props.dispatch,
-                      this.props.marketplace,
-                      this.props.account
-                    );
-                  }}
-                  className="action-button"
-                  style={{ float: "right" }}
-                >
-                  Stop Investment
-                </Button>
-              </div>
-            ) : (
-              <div></div>
-            )}
-
-            {this.props.marketplaceState == "investmentStop" ? (
-              <div className="col">
-                <Button
-                  variant="primary"
-                  onClick={(e) => {
-                    startTicketSale(
-                      this.props.dispatch,
-                      this.props.marketplace,
-                      this.props.account
-                    );
-                  }}
-                  className="action-button"
-                  style={{ float: "right" }}
-                >
-                  Start Tickets Sale
-                </Button>
-              </div>
-            ) : (
-              <div></div>
-            )}
-
-            {this.props.marketplaceState == "ticketSaleStart" ? (
-              <div className="col">
-                <Button
-                  variant="primary"
-                  onClick={(e) => {
-                    startEvent(
-                      this.props.dispatch,
-                      this.props.marketplace,
-                      this.props.account
-                    );
-                  }}
-                  className="action-button"
-                  style={{ float: "right" }}
-                >
-                  Start Event
-                </Button>
-              </div>
-            ) : (
-              <div></div>
-            )}
-
-            {this.props.marketplaceState == "eventStart" &&
-            this.props.investorUnits > 0 ? (
-              <div className="col">
-                <Button
-                  variant="primary"
-                  onClick={(e) => {
-                    retrieve(
-                      this.props.dispatch,
-                      this.props.marketplace,
-                      this.props.account
-                    );
-                  }}
-                  className="action-button"
-                  style={{ float: "right" }}
-                >
-                  Retrieve Investments
-                </Button>
-              </div>
-            ) : (
-              <div></div>
-            )}
-
-            {/* 
-
-
-
-
-
- */}
-
-            {/* show Invest and Ticket components depend on the marketplaseState*/}
-            {/* <div className="row"> */}
-            <Row>
-              <Col>1 of 1</Col>
-              <Col>
-                {/* <div className="col"> */}
-                {this.props.marketplaceState == "investmentStart" ? (
-                  <Invest />
-                ) : (
-                  <div></div>
-                )}
-                {/* </div> */}
-              </Col>
-            </Row>
-            {/* </div> */}
-
+        <Container>
+          {this.props.marketplaceState ? (
             <div className="row">
-              <div className="col">
-                <Tickets />
+              {/* creatingTickets, investmentStart, investmentStop, ticketSaleStart, eventStart  */}
+              {this.props.marketplaceState == "creatingTickets" ? (
+                <div className="col">
+                  <Button
+                    variant="primary"
+                    onClick={(e) => {
+                      startInvestment(
+                        this.props.dispatch,
+                        this.props.marketplace,
+                        this.props.account
+                      );
+                    }}
+                    className="action-button"
+                    style={{ float: "right" }}
+                  >
+                    Start Investment
+                  </Button>
+                </div>
+              ) : (
+                <></>
+              )}
+
+              {this.props.marketplaceState == "investmentStart" ? (
+                <div className="col">
+                  <Button
+                    variant="primary"
+                    onClick={(e) => {
+                      stopInvestment(
+                        this.props.dispatch,
+                        this.props.marketplace,
+                        this.props.account
+                      );
+                    }}
+                    className="action-button"
+                    style={{ float: "right" }}
+                  >
+                    Stop Investment
+                  </Button>
+                </div>
+              ) : (
+                <div></div>
+              )}
+
+              {this.props.marketplaceState == "investmentStop" ? (
+                <div className="col">
+                  <Button
+                    variant="primary"
+                    onClick={(e) => {
+                      startTicketSale(
+                        this.props.dispatch,
+                        this.props.marketplace,
+                        this.props.account
+                      );
+                    }}
+                    className="action-button"
+                    style={{ float: "right" }}
+                  >
+                    Start Tickets Sale
+                  </Button>
+                </div>
+              ) : (
+                <div></div>
+              )}
+
+              {this.props.marketplaceState == "ticketSaleStart" ? (
+                <div className="col">
+                  <Button
+                    variant="primary"
+                    onClick={(e) => {
+                      startEvent(
+                        this.props.dispatch,
+                        this.props.marketplace,
+                        this.props.account
+                      );
+                    }}
+                    className="action-button"
+                    style={{ float: "right" }}
+                  >
+                    Start Event
+                  </Button>
+                </div>
+              ) : (
+                <div></div>
+              )}
+
+              {this.props.marketplaceState == "eventStart" &&
+              this.props.investorUnits > 0 ? (
+                <div className="col">
+                  <Button
+                    variant="primary"
+                    onClick={(e) => {
+                      retrieve(
+                        this.props.dispatch,
+                        this.props.marketplace,
+                        this.props.account
+                      );
+                    }}
+                    className="action-button"
+                    style={{ float: "right" }}
+                  >
+                    Retrieve Investments
+                  </Button>
+                </div>
+              ) : (
+                <div></div>
+              )}
+
+              <Row>
+                <Col>1 of 1</Col>
+                <Col>
+                  {/* <div className="col"> */}
+                  {this.props.marketplaceState == "investmentStart" ? (
+                    <Invest />
+                  ) : (
+                    <div></div>
+                  )}
+                </Col>
+              </Row>
+
+              <div className="row">
+                <div className="col">
+                  <Tickets />
+                </div>
               </div>
             </div>
-          </div>
-        ) : (
-          <Spinner type="table" />
-        )}
-
-        {/* <div className="row">
-          <div className="col">
-            <Tickets />
-          </div>
-        </div> */}
-        {/* </div> */}
+          ) : (
+            <Spinner type="table" />
+          )}
+        </Container>
       </Container>
     );
   }
